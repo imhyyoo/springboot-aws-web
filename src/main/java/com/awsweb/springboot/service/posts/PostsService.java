87e71b160eab11146bd754dafab56c108e1b0880
imhyyoo <imhyyoo@gmail.com>
2021-07-15 21:36:40 +0900

    crud 완료

--- src/main/java/com/awsweb/springboot/service/posts/PostsService.java
+++ src/main/java/com/awsweb/springboot/service/posts/PostsService.java
@@ -4,12 +4,15 @@ import com.awsweb.springboot.domain.posts.Posts;
 import com.awsweb.springboot.domain.posts.PostsRepository;
 import com.awsweb.springboot.web.dto.PostResponseDto;
 import com.awsweb.springboot.web.dto.PostUpdateRequestDto;
+import com.awsweb.springboot.web.dto.PostsListResponseDto;
 import com.awsweb.springboot.web.dto.PostsSaveRequestDto;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,20 @@ public class PostsService {
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         // entity에 해당하는 PostResponseDto를 보내준다.
         return new PostResponseDto(entity);
+    }
+
+    // 등록, 수정, 삭제 가 없이 조회 기능만하는 메서드에 readOnly 속성을 주어서
+    // 조회 속도 향상시킨다
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+        postsRepository.delete(posts);
     }
 }
