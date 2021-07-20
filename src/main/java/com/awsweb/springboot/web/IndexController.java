96efdce31e824312cb698d4b85927500db44ec55
imhyyoo <imhyyoo@gmail.com>
2021-07-20 17:59:00 +0900

    [add] naver oauth2

--- src/main/java/com/awsweb/springboot/web/IndexController.java
+++ src/main/java/com/awsweb/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.awsweb.springboot.web;
 
+import com.awsweb.springboot.config.auth.LoginUser;
 import com.awsweb.springboot.config.auth.dto.SessionUser;
 import com.awsweb.springboot.service.posts.PostsService;
 import com.awsweb.springboot.web.dto.PostResponseDto;
@@ -16,16 +17,16 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 //    @GetMapping("/")
 //    public String index(Model model){
 //        model.addAttribute("posts", postsService.findAllDesc());
 //        return "index";
 //    }
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
